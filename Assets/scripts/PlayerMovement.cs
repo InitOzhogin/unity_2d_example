2872c474cad0a8171b6907e64162fb62b2f0e7cc
sergeyozhogin-mac <sd@runsoft.ru>
2020-06-05 11:49:10 +0500

    init

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    private float x;
+    private float z;
+    private bool rotateX;
+    private float rotationSpeed;
+
+    void Start()
+    {
+        x = 0.0f;
+        z = 0.0f;
+        rotateX = true;
+        rotationSpeed = 75.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump")) {
+            jump = true;
+        }
+
+        Flip(moveX);
+        Anim(moveX);
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+
+    void Flip(float moveX)
+    {
+        if ((moveX < 0 && transform.localScale.x < 0) || (moveX > 0 && transform.localScale.x > 0)) {
+            transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        }
+    }
+
+    void Anim(float moveX)
+    {
+        if (rotateX == true)
+        {
+            x += Time.deltaTime * rotationSpeed;
+
+            if (x > 360.0f)
+            {
+                x = 0.0f;
+                rotateX = false;
+            }
+        }
+        else
+        {
+            z += Time.deltaTime * rotationSpeed;
+
+            if (z > 360.0f)
+            {
+                z = 0.0f;
+                rotateX = true;
+            }
+        }
+
+        transform.localRotation = Quaternion.Euler(x, 0, z);
+    }
+}
